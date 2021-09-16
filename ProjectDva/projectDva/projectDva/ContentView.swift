0abc33d4a50c2499cbbe55f10ecf8af73f937daa
gyseliya <gyseliya@mail.ru>
2021-09-16 10:34:04 +0300

    awe

---
+++ ProjectDva/projectDva/projectDva/ContentView.swift
@@ -0,0 +1,37 @@
+//
+//  ContentView.swift
+//  projectDva
+//
+//  Created by user on 16.09.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack(){
+            Color("gs")
+            VStack(){
+                Text("Hello!")
+                    .foregroundColor(.white)
+                    .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                    .font(.system(size: 50))
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                    Text("Как дела?")
+                })
+                .font(.system(size: 20, weight: .black, design: .monospaced))
+                .frame(width: 200, height: 30, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                
+            }
+            
+        }
+        //.frame(width: UIScreen.main.bounds.width, alignment: .trailing)
+        .ignoresSafeArea()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
